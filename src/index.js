3404c7c5ac53c49038808a716a56a160b7d10778
Farzan Faisal <farzanalif42@gmail.com>
2020-12-31 17:48:08 -0500

    1.3
    Added steps to state. steps only include values that have moved around in the sorting process instead of including the instance of the array that is being sorted in the quicksort algorithm.

--- src/index.js
+++ src/index.js
@@ -2,10 +2,10 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 
-class SortingVis extends React.Component{
-    constructor(props){
+class SortingVis extends React.Component {
+    constructor(props) {
         super(props);
- 
+
         this.state = {
             array: [],
             test: [],
@@ -19,28 +19,30 @@ class SortingVis extends React.Component{
         };
     }
 
-    componentDidMount(){
+    componentDidMount() {
         this.resetArray(this.state.size);
     }
 
-    defaultSort(l){
+    defaultSort(l) {
         let x = duplicateArray(l)
-        x.sort(function(a, b) {
+        x.sort(function (a, b) {
             return a - b;
         });
         return x
     }
 
-    resetArray(){
+
+
+    resetArray() {
         let a = [];
-        for(let i = 0; i < this.state.size; i++){
+        for (let i = 0; i < this.state.size; i++) {
             a.push(randomInt(5, 730));
         }
 
         let t = duplicateArray(a);
         t = this.defaultSort(t);
 
-        for(let i = 0; i < a.length; i++){
+        for (let i = 0; i < a.length; i++) {
             a[i] = [a[i]];
             a[i].push(i);
         }
@@ -57,33 +59,40 @@ class SortingVis extends React.Component{
         });
     }
 
-    addToArray(l, c){
-        if(c.length !== 0){
-            for(let i = 0; i < c.length; i++){
+    addToArray(l, c) {
+        if (c.length !== 0) {
+            for (let i = 0; i < c.length; i++) {
                 l.push(c[i])
             }
         }
         return l;
     }
 
-    quickSortHandler(e, t){
+    quickSortHandler(e, t) {
         let a = this.defaultSort(getValueArray(e));
-        if(getValueArray(e) !== a){
+        if (getValueArray(e) !== a) {
             e = this.quickSort(e);
-            if(JSON.stringify(getValueArray(e)) === JSON.stringify(t)){
+            if (JSON.stringify(getValueArray(e)) === JSON.stringify(t)) {
                 this.setState({
                     array: e
                 });
 
             }
-            else{console.log(this.state, e);}
+            else { console.log(this.state, e); }
         }
+        // console.log(this.state)
+        let s = [];
+        for (let i = 0; i < this.state.history.lesser.length; i++) {
+            s.push(combineSteps(this.state.history.lesser[i], this.state.history.pivot[i], this.state.history.greater[i]))
+        }
+        this.state.steps = s
+        console.log(this.state)
     }
 
-    quickSort(e){
+    quickSort(e) {
         let l = duplicateArray(e)
 
-        if(l.length === 1 || l.length === 0){
+        if (l.length === 1 || l.length === 0) {
             return l
         }
 
@@ -92,19 +101,19 @@ class SortingVis extends React.Component{
         let greater = [];
 
         l.splice(0, 1);
-        
-        for(let i = 0; i < l.length; i++){
-            if(l[i][0] < pivot[0]){
+
+        for (let i = 0; i < l.length; i++) {
+            if (l[i][0] < pivot[0]) {
                 lesser.push(l[i]);
             }
-            else if(l[i][0] >= pivot[0]){
+            else if (l[i][0] >= pivot[0]) {
                 greater.push(l[i]);
             }
         }
 
         let final = []
 
-        
+
         this.state.history.lesser.push(lesser)
         this.state.history.pivot.push(pivot)
         this.state.history.greater.push(greater)
@@ -112,30 +121,30 @@ class SortingVis extends React.Component{
         this.addToArray(final, this.quickSort(lesser))
         final.push(pivot)
         this.addToArray(final, this.quickSort(greater))
-        
+
         return final
     }
 
-    updateSearch(event){
+    updateSearch(event) {
         this.setState({
-            size: event.target.value.substr(0,3)
+            size: event.target.value.substr(0, 3)
         })
     }
 
-    render(){
+    render() {
         const array = getValueArray(this.state.array);
         const mappedArray = array.map((value, idx) => (
-            <div className = "array-bar" key={idx} style={{height: `${value}px`}}></div>
+            <div className="array-bar" key={idx} style={{ height: `${value}px` }}></div>
         ))
-        return(
+        return (
             <div>
-                <div className = "ui-container">
+                <div className="ui-container">
                     <label for="size">Size: </label>
-                    <input type="number" id="size" value={this.state.size} onChange={this.updateSearch.bind(this)}/>
+                    <input type="number" id="size" value={this.state.size} onChange={this.updateSearch.bind(this)} />
                     <button onClick={() => this.resetArray(10)}>New Array</button>
                     <button onClick={() => this.quickSortHandler(this.state.array, this.state.test)}>Quick Sort</button>
                 </div>
-                <div className = "array-container">
+                <div className="array-container">
                     {mappedArray}
                 </div>
             </div>
@@ -143,25 +152,43 @@ class SortingVis extends React.Component{
     }
 }
 
-function getValueArray(l){
+function combineSteps(a, b, c) {
+    let d = []
+    function enterList(l, c) {
+        if (typeof (c[0]) === "number") {
+            l.push(c)
+        }
+        else {
+            for (let i = 0; i < c.length; i++) {
+                l.push(c[i])
+            }
+        }
+    }
+    enterList(d, a)
+    enterList(d, b)
+    enterList(d, c)
+    return d
+}
+
+function getValueArray(l) {
     let a = [];
-    for(let i = 0; i < l.length; i++){
+    for (let i = 0; i < l.length; i++) {
         a.push(l[i][0]);
     }
     return a;
 }
 
-function duplicateArray(l){
+function duplicateArray(l) {
     let x = [];
-    for(let i = 0; i < l.length; i++){x.push(l[i]);}
+    for (let i = 0; i < l.length; i++) { x.push(l[i]); }
     return x;
 }
 
-function randomInt(a, b){
+function randomInt(a, b) {
     return Math.floor(a + Math.random() * (1 + b - a));
 }
 
 ReactDOM.render(
-    <SortingVis/>,
+    <SortingVis />,
     document.getElementById('root')
 )
