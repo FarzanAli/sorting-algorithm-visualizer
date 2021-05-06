3d5390fac890e6c86338ce399ac02ab645c46ffb
Farzan Faisal <farzanalif42@gmail.com>
2021-05-05 22:03:44 -0400

    Update

--- src/components/UI.js
+++ src/components/UI.js
@@ -2,9 +2,13 @@ import React, {Component} from 'react';
 
 export default class UI extends Component{
     render(){
+
         return(
             <div className="ui-container">
-                <input type="number" className="sizeInput" value={this.props.size} onChange={this.props.updateSearch} />
+                <div className="size-container">
+                    <label for="size" id="sizeLabel">Size:</label>
+                    <input type="number" className="sizeInput" value={this.props.size} onChange={this.props.updateSearch} />
+                </div>
                 <button className="button" onClick={this.props.resetArray}>New Array</button>
                 <button className="button" onClick={this.props.iterateSteps}>Quick Sort</button>
             </div>
