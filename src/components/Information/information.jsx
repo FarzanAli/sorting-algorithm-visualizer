af4576cef2b68d74160d7e6774307a77e58edb43
Farzan Faisal <farzanalif42@gmail.com>
2022-03-23 23:18:20 -0400

    added bars

--- src/components/Information/information.jsx
+++ src/components/Information/information.jsx
@@ -1,8 +1,9 @@
 import React from 'react';
+import './information.css';
 
 let Information = (props) => {
     return(
-        <div className='information-container' style={{width: 'fit-content'}}>
+        <div className='information-container'>
             {props.name}
         </div>
     );
--- src/components/visualizer/visualizer.jsx
+++ src/components/visualizer/visualizer.jsx
@@ -1,9 +1,26 @@
-import React from 'react';
+import React, {useState} from 'react';
+import './visualizer.css';
 
 let Visualizer = (props) => {
+    const [array, setArray] = useState([10,20]);
+    
     return(
-        <div className='visualizer-container' style={{marginRight: '50px'}}>
-            array bars here
+        <div className='visualizer-container'>
+            <div className='bar-container'>
+                {array.map((value, idx) => 
+                    <div
+                    className='bar'
+                    key={idx}
+                    style={{
+                            display: 'inline-block',
+                            margin: '3px',
+                            height: value.toString() + 'px',
+                            width: '8px',
+                            backgroundColor: 'black'            
+                        }}
+                    ></div>
+                )}
+            </div>
         </div>
     );
 }
